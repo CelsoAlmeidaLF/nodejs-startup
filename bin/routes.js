84867deae7f4c15f7d3af5ac0e18a9c4a6b95914
Celso de Almeida Leite Filho <celso.almeida.leite@hotmail.com>
2020-08-24 14:15:42 -0300

    init nodejs-startup.

--- bin/routes.js
+++ bin/routes.js
@@ -2,6 +2,9 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function(req, res) { res.render('home/index') })
+router.get('/servicos', function(req, res) { res.render('home/servicos') })
+router.get('/sobre', function(req, res) { res.render('home/sobre') })
+router.get('/contato', function(req, res) { res.render('home/contato') })
 
 router.use(function(req, res, next) { res.status(404).redirect('/') })
 
